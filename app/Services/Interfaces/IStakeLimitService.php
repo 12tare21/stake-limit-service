e9fb0011633387018d5c18859888304ce951bb60
Tarik Muminović <12tare21@gmail.com>
2020-11-29 21:39:06 +0100

    fix(service): interface expanded with public methods ready for testin (#25)

--- app/Services/Interfaces/IStakeLimitService.php
+++ app/Services/Interfaces/IStakeLimitService.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 interface IStakeLimitService{
     function recieveTicketMessage(Request $request);
     function configureStakeLimit(Request $request);
+    function deviceBlocked($stakeSum, $deviceId, $considerExpiration);
+    function resolveDeviceStatus($stakeSum, $deviceId, $considerExpiration);
+    function stakeSumByDevice($deviceId);
 }
